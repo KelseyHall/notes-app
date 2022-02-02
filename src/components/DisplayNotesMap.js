b6465eebab431ff8b8e52f3eb3094c9a3e4358a9
Kelsey H <kels-hall23@hotmail.com>
2022-02-02 10:19:32 -0500

    card to note

--- src/components/DisplayNotesMap.js
+++ src/components/DisplayNotesMap.js
@@ -1,15 +1,17 @@
-import { Button } from '@mui/material';
+import { Button, Card } from '@mui/material';
 import { DeleteNote } from './Function';
 import DeleteIcon from '@mui/icons-material/Delete';
 const DisplayNotes = ({ Notes, setNotes }) => {
   return Notes.map(({ id, Note }) => {
     return (
-      <p key={id}>
-        {Note}
-        <Button onClick={() => DeleteNote(id, Notes, setNotes)}>
-          <DeleteIcon />
-        </Button>
-      </p>
+      <Card>
+        <p key={id}>
+          {Note}
+          <Button onClick={() => DeleteNote(id, Notes, setNotes)}>
+            <DeleteIcon />
+          </Button>
+        </p>
+      </Card>
     );
   });
 };
